42d40f6332d0db234ccf12b1dfe830f1705f54e3
Charles Cruse <charle9@vt.edu>
2023-02-22 16:04:37 -0500

    completed validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,11 +6,18 @@ RSpec.describe User, type: :model do
   it {should validate_presence_of(:password_digest)}
   it {should validate_length_of(:password_digest).is_at_least(6)}
 
-  describe 'uniqueness' do
-    before :each do
-      create (:user)
-    end
-  end
+  # describe 'uniqueness' do
+  #   before :each do
+  #     create (:user)
+  #   end
+  # end
+
+  subject(:user) {
+    User.new(
+      username: 'A',
+      password_digest: '123456'
+    )
+  }
 
   it {should validate_uniqueness_of(:username)}
 end
