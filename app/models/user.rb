b770eac6c52232db4437b5663553b67cb038aac1
Amir Lamrabat <secretsofthecode9@gmail.com>
2023-02-22 15:16:53 -0500

    validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+
+    validates :username, presence: true, uniqueness: true 
+    validates :password_digest, presence: true 
+    validates :password_digest, length: {minimum:6}, allow_nil: true 
 end
